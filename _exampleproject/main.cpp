7db8d73c17915c0b69d72de57b7368d714f3fd24
Alex Jaeger <h.jaeger@umail.ucc.ie>
2014-08-11 16:17:32 +0100

    Modified maim.cpp

--- _exampleproject/main.cpp
+++ _exampleproject/main.cpp
@@ -4,25 +4,25 @@ extern "C" int main(void)
 {
 #if !defined(ARDUINO)
 
-	// To use Teensy 3.0 without Arduino, simply put your code here.
-	// For example:
+    // To use Teensy 3.0 without Arduino, simply put your code here.
+    // For example:
 
-	pinMode(13, OUTPUT);
-	while (1) {
-		digitalWriteFast(13, HIGH);
-		delay(500);
-		digitalWriteFast(13, LOW);
-		delay(500);
-	}
+    pinMode(13, OUTPUT);
+    while (1) {
+        digitalWriteFast(13, HIGH);
+        delay(500);
+        digitalWriteFast(13, LOW);
+        delay(500);
+    }
 
 
 #else
-	// Arduino's main() function just calls setup() and loop()....
-	setup();
-	while (1) {
-		loop();
-		yield();
-	}
+    // Arduino's main() function just calls setup() and loop()....
+    setup();
+    while (1) {
+        loop();
+        yield();
+    }
 #endif
 }
 
